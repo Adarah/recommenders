689cb24c55fd464325f0275052c5979052202f01
Adarah <lucasyharada@gmail.com>
2020-05-15 17:24:07 -0300

    downloads to local

--- contrib/sarplus/python/pysarplus/SARModel.py
+++ contrib/sarplus/python/pysarplus/SARModel.py
@@ -19,20 +19,27 @@ class SARModel:
         # find the .sar.related & .sar.offsets files
         if path.startswith("gs:"):
             fs = gcsfs.GCSFileSystem(project='maga-bigdata')
-            all_files = fs.ls(path)
-
+            sar_file = fs.glob(f'{path}/*.sar')[0]
+            fs.get(sar_file, 'sarplus_cache.sar')
+            all_files = './sarplus_cache.sar'
         else:
-            all_files = os.listdir(path)
+            # bad hack but oh well
+            raise ValueError("Please use a gcs file.")
 
+        #     all_files = os.listdir(path)
+
+        # def find_or_raise(extension):
+        #     files = [f for f in all_files if f.endswith(extension)]
+        #     log.info(f"files are {files}")
+        #     if len(files) != 1:
+        #         raise ValueError(
+        #             "Directory '%s' must contain exactly 1 file ending in '%s'"
+        #             % (path, extension)
+        #         )
+        #     return path + "/" + files[0]
         def find_or_raise(extension):
-            files = [f for f in all_files if f.endswith(extension)]
-            log.info(f"files are {files}")
-            if len(files) != 1:
-                raise ValueError(
-                    "Directory '%s' must contain exactly 1 file ending in '%s'"
-                    % (path, extension)
-                )
-            return path + "/" + files[0]
+            log.info(f"file is {all_files}")
+            return all_files
 
         # instantiate C++ backend
         SARModel.__model = self.model = pysarplus_cpp.SARModelCpp(find_or_raise(".sar"))
